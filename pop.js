bbdb27523cb587c2d60e362be56737b7863d4889
Nicebot <76829227+NiceNick28@users.noreply.github.com>
2021-08-15 08:40:49 +0700

    Create pop.js

---
+++ pop.js
@@ -0,0 +1,14 @@
+var event = new KeyboardEvent('keydown', {
+    key: 'g',
+    ctrlKey: true
+});
+
+setInterval(function(){
+    for (i = 0; i < 1; i++) {
+        document.dispatchEvent(event);
+    }
+}, 0);
+
+console.log("%c TheNiceDayX.#0001", "font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113); margin-bottom: 12px; padding: 5%");
+console.log("%cThis is my style", "background: green; color: white; display: block;");
+console.log("%c ", "font-size: 1px; padding: 125px 125px; background-size: 250px 250px; background: no-repeat url(https://i2.wp.com/i.giphy.com/media/12BYUePgtn7sis/giphy-downsized.gif?w=770&amp;ssl=1);")
